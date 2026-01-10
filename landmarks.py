d77a53d11ab6e1933b11f7812b22a418ae7c5fe3
Zhi <zhihongchen.work@gmail.com>
2026-01-10 03:19:01 -0500

    Updated

--- landmarks.py
+++ landmarks.py
@@ -1,10 +1,7 @@
-landmarks = {
-    "Aral_Sea": "43,57,47,61",
-    "Dubai_Palm_Islands": "24.9,54.8,25.2,55.2",
-    "Amazon_Deforestation": "-11,-64,-8,-61",
-    "Las_Vegas_Growth": "35.8,-115.5,36.4,-114.8",
-    "Grand_Canyon": "35.8,-114.0,36.5,-112.0" 
-}
+from database import load_landmarks, save_landmarks
+
+landmarks = load_landmarks()
+
 
 def print_landmarks():
     print("\nYour current landmarks consist of: ")
@@ -26,4 +23,5 @@ def add_new_landmark():
         
         bbox_string = f"{min_lat},{min_lon},{max_lat},{max_lon}"
         landmarks[name] = bbox_string
+        save_landmarks(landmarks)
         print(f"Added {name} successfully!")
